d64fe8684cecb1ba0b6e08372ace89201c454674
tad dy <a@a.com>
2024-08-03 10:49:22 -0400

    updating the README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex flex-col min-h-screen justify-center items-center p-24">
-      <h1 className="text-5xl cursor-pointer" onClick={() => {}}>
-        R.A.G. w/ Pinecone
-      </h1>
+    <main className="flex flex-col min-h-screen justify-center items-center p-24 space-y-4">
+      <h1 className="text-5xl cursor-pointer">R.A.G. w/ Pinecone</h1>
+      <button
+        onClick={() => {
+          router.push("/agents/rag");
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Enter
+      </button>
     </main>
   );
 }
