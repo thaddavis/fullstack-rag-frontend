5a7cc75aa69862806958b9b4602b7c750ff34a0b
tad dy <a@a.com>
2024-08-06 17:14:01 -0400

    allow jwt to rehydrate application across reloads

--- src/app/dashboard/recommendations/page.tsx
+++ src/app/dashboard/recommendations/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <div className="mx-auto max-w-2xl px-4">
         <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
           <h1 className="text-center text-5xl font-semibold leading-12">
-            &quot;Recommendations&quot;
+            Recommendations
           </h1>
         </div>
       </div>
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -2,28 +2,52 @@
 
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 const AuthContext = createContext<{
   user: any;
+  setUser: (arg: any) => void;
   login: (username: string, password: string) => void;
   logout: () => void;
 }>({
   user: null,
+  setUser: () => {},
   login: () => {},
   logout: () => {},
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
   const router = useRouter();
+
+  useEffect(() => {
+    // Rehydrate user from localStorage
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      // Assuming you have an endpoint to validate the token and get user info
+      axios
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/auth/validate-token`)
+        .then((response) => {
+          console.log("response", response);
+          setUser(response.data);
+        })
+        .catch((error) => {
+          console.error("Rehydration Failed:", error);
+          // Optionally, remove the token if invalid
+          localStorage.removeItem("token");
+          setUser(undefined);
+        });
+    }
+  }, []);
 
   const login = async (username: string, password: string) => {
     try {
       const formData = new FormData();
       formData.append("username", username);
       formData.append("password", password);
+
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/auth/token`,
         formData,
@@ -36,11 +60,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       ] = `Bearer ${response.data.access_token}`;
       localStorage.setItem("token", response.data.access_token);
 
-      // debugger;
-
       setUser(response.data);
+
       router.push("/dashboard");
     } catch (error) {
+      setUser(undefined);
+
       toast.error("Login Failed");
 
       console.error("Login Failed:", error);
@@ -48,15 +73,15 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   };
 
   const logout = () => {
-    setUser(null);
-    delete axios.defaults.headers.common["Authorization"];
+    setUser(undefined);
+    localStorage.removeItem("token");
     router.push("/");
   };
 
   console.log("auth-context.tsx: user:", user);
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, setUser, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
