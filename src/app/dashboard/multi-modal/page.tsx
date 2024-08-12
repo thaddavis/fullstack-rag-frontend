8385b000147135fa90eb87eb7130bece2f975841
tad dy <a@a.com>
2024-08-12 17:55:08 -0400

    delete ProtectedRoute wrapper

--- src/app/dashboard/multi-modal/page.tsx
+++ src/app/dashboard/multi-modal/page.tsx
@@ -1,15 +1,11 @@
-import ProtectedRoute from "@/components/shared/protected-route";
-
 export default function Page() {
   return (
-    <ProtectedRoute>
-      <div className="mx-auto max-w-2xl px-4">
-        <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
-          <h1 className="text-center text-5xl font-semibold leading-12 text-ellipsis overflow-hidden">
-            Multi-Modal R.A.G.
-          </h1>
-        </div>
+    <div className="mx-auto max-w-2xl px-4">
+      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
+        <h1 className="text-center text-5xl font-semibold leading-12 text-ellipsis overflow-hidden">
+          Multi-Modal R.A.G.
+        </h1>
       </div>
-    </ProtectedRoute>
+    </div>
   );
 }
